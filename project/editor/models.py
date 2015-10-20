135221e1f59554a55d4e476f42d614b5c120e9fa
HBCompass <heather@rebelcompass.com>
2015-10-20 11:19:31 -0700

    preliminary classes

--- project/editor/models.py
+++ project/editor/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+
+class Story(models.Model):
+    """A story contains universal information for a specific story"""
+    pass
+
+
+class WebForm(models.Model):
+    """ The web article version of a story."""
+    pass
+
+
+class RadioForm(models.Model):
+    """ The radio version of a story."""
+    pass
+
+class TvForm(models.Model):
+    """ The television version of a story."""
+    pass
+
+
+class PrintForm(models.Model):
+    """ The print version of a story."""
+    pass
+
+
+# future thinking: should every iteration of a platform have it's own class?
+# OR: subclass newspaper and magazine under PrintForm...
+
