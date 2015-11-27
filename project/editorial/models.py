be6cec3becda6dc4d97a59cdb34f040d9580a532
HBCompass <heather@rebelcompass.com>
2015-11-27 09:28:29 -0800

    expanded user and organization classes

--- project/editorial/models.py
+++ project/editorial/models.py
@@ -15,15 +15,19 @@ from imagekit.models import ProcessedImageField
 # from django.utils import timezone
 # from postgres.fields import ArrayField
 
-# Models
-
 #----------------------------------------------------#
-# Users
+#   People
 #----------------------------------------------------#
 
 
 class User(models.Model):
-    """ Site User """
+    """ Site User.
+
+    A user can either be an admin or a general user. Most users
+    can do most things. An admin user can be a site owner, add new
+    users, create and manage networks and shift users from active
+    to inactive. A general user creates and collaborates on content.
+    """
 
     user_id = models.SlugField(
         max_length=15,
@@ -35,6 +39,7 @@ class User(models.Model):
 
     user_admin_privilege = models.BooleanField(
         initial = false
+        help_text='Is a user able to manage an organization/network and make/remove users.'
         )
 
     user_fname = models.CharField(
@@ -47,11 +52,22 @@ class User(models.Model):
         db_index=True,
         )
 
+    user_credit_name = models.CharField(
+        max_length=75,
+        help_text='Full name of user as listed as a credit on content.'
+    )
+
     username = models.CharField(
-        max_length=30
+        max_length=30,
+        unique=True,
+        help_text='Username as needed for login purposes.'
         )
 
-    # user_password 
+    user_title = models.CharField(
+        max_length=100,
+        unique=True,
+        help_text='Professional title'
+    )
 
     user_date_joined = models.DateTimeField(
         auto_now_add=True
@@ -90,38 +106,58 @@ class User(models.Model):
         format='JPEG',
         )
 
+    #Links to user's professional social media accounts
     user_facebook = models.CharField(
-        max_length=75
+        max_length=150
         )
 
     user_twitter = models.CharField(
-        max_length=75
+        max_length=150
     )
 
     user_linkedin = models.CharField(
-        max_length=75
+        max_length=150
     )
 
     user_instagram = models.CharField(
-        max_length=75
+        max_length=150
     )
 
     user_snapchat = models.CharField(
-        max_length=75
+        max_length=150
     )
 
     user_vine = models.CharField(
-        max_length=75
+        max_length=150
     )
+
+    class Meta:
+        verbose_name= = 'Team member'
+        verbose_name_plural = "Team members"
+        ordering = ['user_credit_name']
+
+    def __str__(self):
+        return self.user_credit_name
+
+    @property
+    def description(self):
+        return "{user}, {title}".format(user=self.user_credit_name, title=self.user_title)
 
 
 class Organization(models.Model)
-""" Media Organization """
+    """ Media Organization.
+
+    An organization is a media or publishing entity. Organization's are created
+    and owned by one admin user. They can be managed by multiple admin users.
+    Organizations have many users and serve as the owner of story content. Organizations
+    can create and manage Networks. Ownership of an organization can be transferred
+    from one admin user to another.
+    """
 
     organization_id = models.SlugField(
         max_length=15,
         primary_key=True,
-        help_text='Unique code for a user.'
+        help_text='Unique code for an organization.'
         )
 
     organization_name = models.CharField(
@@ -151,14 +187,34 @@ class Organization(models.Model)
         format='JPEG',
     )
 
+    class Meta:
+        verbose_name= = 'Organization'
+        verbose_name_plural = "Organizations"
+        ordering = ['organization_name']
+
+    def __str__(self):
+        return self.organization_name
+
+    @property
+    def description(self):
+        return "{organization}, {description}".format(organization=self.organization_name, description=self.organization_description)
+
 
 class Network(models.Model)
-""" A group of organizations. """
+    """ A group of organizations.
+
+    A network is a collection of two or more organizations seeking to create a sharing
+    or collaborating relationship. Sharing means the organization can add content that has been
+    marked for sharing to that network. Collaborating means that any user from the guest network
+    organization(s) can participate in the editorial process on the host organization's content that has been
+    selected as collaborative. At the conclusion of the editorial process, the guest can add the final
+    version of the content to their own account.
+    """
 
     network_id = models.SlugField(
         max_length=15,
         primary_key=True,
-        help_text='unique identifier for a network'
+        help_text='Unique identifier for a network'
         )
 
     network_owner_organization = models.ForeignKey(Organization)
@@ -166,6 +222,7 @@ class Network(models.Model)
     network_name = models.CharField(
         max_length=75,
         db_index=True,
+        help_text="The name by which members identify the network."
         )
 
     network_creation_date = models.DateTimeField(
@@ -188,9 +245,20 @@ class Network(models.Model)
         format='JPEG',
     )
 
+    class Meta:
+        verbose_name= = 'Network'
+        verbose_name_plural = "Networks"
+        ordering = ['network_name']
+
+    def __str__(self):
+        return self.network_name
+
+    @property
+    def description(self):
+        return "{network}, {description}".format(network=self.network_name, description=self.network_description)
 
 
-
+class NetworkOrganizaton()
 
 # #----------------------------------------------------#
 # # Content
