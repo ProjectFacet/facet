daf4d234e8d305bef8a927b40c598e3b48dfac10
HBCompass <heather@rebelcompass.com>
2015-12-03 18:59:07 -0800

    updating urls

--- project/editorial/views.py
+++ project/editorial/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http impor HttpResponse
+from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
+def index(request):
     # return static homepage for now
     return render(request, 'editorial/home.html')
--- project/project/urls.py
+++ project/project/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from editorial import views
+
 
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('editorial.urls')),
 ]
