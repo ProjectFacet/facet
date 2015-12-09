aa4dbbf6c3fdbc19ae53ace014b4fc417b2a47ff
HBCompass <heather@rebelcompass.com>
2015-12-09 15:18:33 -0800

    moved app-specific urls to app urls

--- project/editorial/urls.py
+++ project/editorial/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^story/new/$', views.story_new, name='story_new'),
+    url(r'^user/profile/edit/', views.edit_user, name='edit_user')
 ]
--- project/project/urls.py
+++ project/project/urls.py
@@ -25,6 +25,4 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include(allauth.urls)),
     url(r'', include('editorial.urls')),
-    url(r'^story/new/$', views.story_new, name='story_new'),
-    url(r'^user/profile/edit/', views.edit_user, name='edit_user')
 ]
