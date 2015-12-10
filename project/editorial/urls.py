5c3e14adbf5e609273aeb628725cea22a2c37cb2
HBCompass <heather@rebelcompass.com>
2015-12-10 02:31:08 -0800

    minor changes to html and comments on urls.py

--- project/editorial/urls.py
+++ project/editorial/urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
+    # homepage
     url(r'^$', views.index, name='index'),
+    # user profile
+    url(r'^user/profile/edit/', views.edit_user, name='edit_user'),
+    # make/edit a series
+    url(r'^series/new$', views.series_new, name='series_new'),
+    # make/edit a story
     url(r'^story/new/$', views.story_new, name='story_new'),
-    url(r'^user/profile/edit/', views.edit_user, name='edit_user')
 ]
