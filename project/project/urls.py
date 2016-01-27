ab986232a2e6f53acc5fbdc884c672bfeaa492f1
HBCompass <heather@rebelcompass.com>
2016-01-26 20:02:15 -0800

    org logos working

--- project/project/urls.py
+++ project/project/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from editorial import views
@@ -27,4 +29,4 @@ urlpatterns = [
     url(r'^tinymce/', include('tinymce.urls')),
     # url(r'^activity/', include('actstream.urls')),
     url(r'', include('editorial.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
