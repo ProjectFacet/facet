3af78c93925f8c361a107198c47560738659071c
Katie <ktel1218@gmail.com>
2016-02-26 10:48:23 -0800

    commented out incomplete search added migration

--- project/search.py
+++ project/search.py
@@ -1,27 +1,27 @@
-"""Search feature via watson."""
+# """Search feature via watson."""
 
-from watson import search as watson
+# from watson import search as watson
 
-class SearchAdapter(watson.SearchAdapter):
+# class SearchAdapter(watson.SearchAdapter):
 
-    def get_title(self, obj):
-        """ Returns search_title of the search result."""
+#     def get_title(self, obj):
+#         """ Returns search_title of the search result."""
 
-        return obj.search_title
+#         return obj.search_title
 
-    def get_description(self, obj):
-        """ Returns description of the search result."""
+#     def get_description(self, obj):
+#         """ Returns description of the search result."""
 
-        return obj.description
+#         return obj.description
 
-    def get_absolute_url(self, obj):
-        """ Returns url of instance or instance container."""
+#     def get_absolute_url(self, obj):
+#         """ Returns url of instance or instance container."""
 
-        return obj.get_absolute_url
+#         return obj.get_absolute_url
 
 
-def register_watson(app, model_name):
-    """Register a model with the search."""
+# def register_watson(app, model_name):
+#     """Register a model with the search."""
 
-    watson.register(app.get_model(model_name),
-        SearchAdapter,)
+#     watson.register(app.get_model(model_name),
+#         SearchAdapter,)
