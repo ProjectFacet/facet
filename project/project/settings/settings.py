723154127f2ce441d7be7a6aaa72c507b867de7f
Katie <ktel1218@gmail.com>
2016-03-09 09:25:49 -0800

    changed tinymce root to grab STATIC_URL

--- project/project/settings/settings.py
+++ project/project/settings/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE_CLASSES = (
     'simple_history.middleware.HistoryRequestMiddleware',
 )
 
-TINYMCE_JS_ROOT = '/static/scripts/tiny_mce/'
+TINYMCE_JS_ROOT = os.path.join(STATIC_URL, 'scripts/tiny_mce/')
 TINYMCE_JS_URL = os.path.join(STATIC_URL, 'scripts/tiny_mce/tinymce.min.js')
 TINYMCE_DEFAULT_CONFIG = {
     'plugins': "spellchecker,paste,searchreplace,wordcount",
