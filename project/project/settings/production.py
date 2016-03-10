4fea7c46d42af834fc0500b810708c15f14099d5
Katie <ktel1218@gmail.com>
2016-03-10 09:19:38 -0800

    removing addition of 'static' into s3 filepath, not ready for it

--- project/project/settings/production.py
+++ project/project/settings/production.py
@@ -41,8 +41,7 @@ if unset_secrets:
     raise KeyError, "There are unset keys. Use `$ eb setenv KEY=value` to set."
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-STATICFILES_LOCATION = 'static'
-STATIC_URL = "https://%s/%s" % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 
 TINYMCE_JS_ROOT = os.path.join(STATIC_URL, 'scripts/tiny_mce/')
 TINYMCE_JS_URL = os.path.join(STATIC_URL, 'scripts/tiny_mce/tinymce.min.js')
