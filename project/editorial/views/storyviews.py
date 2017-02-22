ec5536a9d1ca797626520de2a17ead72b93d1c9b
HBCompass <heather@rebelcompass.com>
2017-02-22 15:11:07 -0800

    updated story views

--- project/editorial/views/storyviews.py
+++ project/editorial/views/storyviews.py
@@ -444,9 +444,6 @@ def story_detail(request, pk):
         videofacet_audio = []
         videofacet_video = []
 
-    print "pfi: ", printfacet_images
-    print "pfv: ", printfacet_video
-
     storydownloadform = StoryDownloadForm(story=story)
 
     return render(request, 'editorial/storydetail.html', {
