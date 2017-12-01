da914b7fdd8e2e34b1f53a0c7015a7ae61f1a42e
Heather <heather@rebelcompass.com>
2017-11-30 23:31:33 -0800

    created basic forms for contractors and contract content

---
+++ project/editorial/forms/contractorforms.py
@@ -0,0 +1,136 @@
+"""Forms for Contractors and related entities.
+
+    Contractor
+    Call
+    Pitch
+    Assignment
+
+"""
+
+import datetime
+from bootstrap3_datetime.widgets import DateTimePicker
+from .customwidgets import OurDateTimePicker, ArrayFieldSelectMultiple
+from django import forms
+from django.utils.safestring import mark_safe
+from django.contrib.auth import get_user_model
+from django.forms import Textarea, TextInput, RadioSelect, Select, NumberInput, CheckboxInput, CheckboxSelectMultiple, FileField
+from django.contrib.postgres.fields import ArrayField
+from datetimewidget.widgets import DateTimeWidget
+from tinymce.widgets import TinyMCE
+# from django.contrib.staticfiles.templatetags.staticfiles import static
+
+
+from editorial.models import (
+    User,
+    Organization,
+    ContributorInfo,
+    Call,
+    Pitch,
+    Assignment,
+    SimpleImage,
+    SimpleDocument,
+    SimpleAudio,
+    SimpleVideo,
+)
+
+
+class ContractorProfileForm(forms.ModelForm):
+    """Handles creation and editing of a contractor's profile."""
+
+    class Meta:
+        model = ContractorInfo
+        fields = [
+            'resume',
+            'address',
+            'availability',
+            'current_location',
+            'gear',
+        ]
+        widgets = {
+            'address': Textarea(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+            'availability': TextInput(attrs={'class': 'form-control', 'placeholder': 'Availability'}),
+            'current_location': TextInput(attrs={'class': 'form-control', 'placeholder': 'Current Location'}),
+            'gear': TextInput(attrs={'class': 'form-control', 'placeholder': 'Gear'}),
+        }
+
+
+class OrganizationContractorRelationshipForm(forms.ModelForm):
+    """Handles creation and editing of the details of a contractor's
+    relationship with a specific organization.
+    """
+
+    class Meta:
+        model = OrganizationContractorInfo
+        fields = [
+            'w9_on_file',
+            'rates',
+            'strengths',
+            'conflicts',
+            'editor_notes',
+            'talent_pool',
+            'status',
+        ]
+        widgets = {
+            'rates': TextInput(attrs={'class': 'form-control', 'placeholder': 'Rates'}),
+            'strengths': TextInput(attrs={'class': 'form-control', 'placeholder': 'Strengths'}),
+            'conflicts': TextInput(attrs={'class': 'form-control', 'placeholder': 'Conflicts'}),
+            'editor_notes': TextInput(attrs={'class': 'form-control', 'placeholder': 'Editor Notes'}),
+            'status': Select(attrs={'class': 'c-select', 'id':'contractor-status'}),
+        }
+
+
+class CallForm(forms.ModelForm):
+    """Handles creation and editing of a call."""
+
+    class Meta:
+        model = Call
+        fields = [
+            'name',
+            'text',
+            'expiration_date',
+            'is_active',
+            'urgent',
+            'timeframe',
+            'status',
+        ]
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Text'}),
+            'timeframe': TextInput(attrs={'class': 'form-control', 'placeholder': 'Timeframe'}),
+            'status': Select(attrs={'class': 'c-select', 'id':'call-status'}),
+        }
+
+
+class PitchForm(forms.ModelForm):
+    """Handles creation and editing of a pitch."""
+
+    class Meta:
+        model = Pitch
+        fields = [
+            'name',
+            'text',
+            'status',
+            'exclusive',
+        ]
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Text'}),
+            'status': Select(attrs={'class': 'c-select', 'id':'pitch-status'}),
+        }
+
+
+class AssignmentForm(forms.ModelForm):
+    """Handles creation and editing of a assignment."""
+
+    class Meta:
+        model = Assignment
+        fields = [
+            'name',
+            'text',
+            'rate',
+        ]
+        widgets = {
+            'name': TextInput(attrs={'class': 'form-control', 'placeholder': 'Name'}),
+            'text': Textarea(attrs={'class': 'form-control', 'placeholder': 'Text'}),
+            'rate': TextInput(attrs={'class': 'form-control', 'placeholder': 'Rates'}),
+        }
