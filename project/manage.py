af12116b47fdc87ec34836b6624a735cc7c2f07f
Heather <heather@rebelcompass.com>
2018-01-04 13:15:04 -0800

    Add deployment-style env reading.

--- project/manage.py
+++ project/manage.py
@@ -3,6 +3,12 @@ import os
 import sys
 
 if __name__ == "__main__":
+    try:
+        # on production, try to load environmental secrets
+        import env
+    except ImportError:
+        pass
+
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.development")
 
     from django.core.management import execute_from_command_line
--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -10,6 +10,11 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
+try:
+    import env
+except ImportError:
+    pass
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.development")
 
 application = get_wsgi_application()
