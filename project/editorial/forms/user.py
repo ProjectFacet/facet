bbd2db09aa4382d02006a0e88abbc5e271454ad2
Heather <heather@rebelcompass.com>
2018-04-12 00:38:11 -0700

    add pronoun and pronunciation to form and templates

--- project/editorial/forms/user.py
+++ project/editorial/forms/user.py
@@ -35,6 +35,8 @@ class UserProfileForm(forms.ModelForm):
             'first_name',
             'last_name',
             'credit_name',
+            'pronoun',
+            'name_pronunciation',
             'title',
             'phone',
             'email',
@@ -51,13 +53,17 @@ class UserProfileForm(forms.ModelForm):
                 attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
             'credit_name': TextInput(
                 attrs={'class': 'form-control', 'placeholder': 'Credit Name'}),
+            'pronoun': TextInput(
+                attrs={'class': 'form-control', 'placeholder': 'Preferred Pronouns'}),
+            'name_pronunciation': Textarea(
+                attrs={'class': 'form-control', 'rows': 1, 'placeholder': 'How do you pronounce your name?'}),
             'title': TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
             'location': TextInput(attrs={'class': 'form-control', 'placeholder': 'Location'}),
             'email': TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
             'phone': TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}),
             'website': TextInput(attrs={'class': 'form-control', 'placeholder': 'Website'}),
             'bio': Textarea(
-                attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Professional Bio'}),
+                attrs={'class': 'form-control', 'rows': 6, 'placeholder': 'Professional Bio'}),
             'expertise': Textarea(
-                attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Expertise'}),
+                attrs={'class': 'form-control', 'rows': 6, 'placeholder': 'Expertise'}),
         }
